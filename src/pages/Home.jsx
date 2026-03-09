49c544a49fb6c8f589f20f840801906fdb6a003e
Irene Rubio Ramos <ireneramos.code@gmail.com>
2026-03-09 14:06:35 +0100

    Creado snippet sobre nosotros y ajustado HeroSection principal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import AboutSnippet from "../components/AboutSnippet";
 import HeroSection from "../components/HeroSection";
 
 export default function Home() {
     return (
         <>
             <HeroSection />
+            <AboutSnippet />
         </>
     );
 }
