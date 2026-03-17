7aa06ab9a872efb314f911f9e07a7ee965a7e365
Irene Rubio Ramos <ireneramos.code@gmail.com>
2026-03-17 19:24:36 +0100

    Hechos primeros dos componentes de sobre nosotros

--- src/pages/SobreNosotros.jsx
+++ src/pages/SobreNosotros.jsx
@@ -1,3 +1,11 @@
+import AboutIntro from "../components/nuestrosPeludos/AboutIntro";
+import AboutMission from "../components/nuestrosPeludos/AboutMission";
+
 export default function SobreNosotros() {
-    return <div>Sobre Nosotros</div>
+    return (
+        <>
+            <AboutIntro />
+            <AboutMission />
+        </>
+    )
 }
